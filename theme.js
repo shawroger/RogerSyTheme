41bd391d8059a1a94a3e74b93c18c6ef31545b54
shawroger <roger@shawroger.com>
2022-09-05 09:03:04 +0800

    add addRenderNoteRoute

--- theme.js
+++ theme.js
@@ -228,16 +228,14 @@ function ViewMonitor(event) {
 setTimeout(() => ClickMonitor(), 1000);
 
 /* inject local script */
-function inject() {
+(function inject() {
 	const script = document.querySelector("#emojiScript");
 	const js = document.createElement("script");
 	js.setAttribute("src", "./appearance/themes/RogerSyTheme/comment/index.js");
 	js.setAttribute("type", "module");
 	js.setAttribute("defer", "defer");
 	document.head.insertBefore(js, script);
-}
-
-inject();
+})();
 
 function forceReload() {
 	try {
@@ -255,3 +253,88 @@ window.addEventListener("keydown", (event) => {
 		forceReload();
 	}
 });
+
+function getHPathByPath(data) {
+	return request("/api/filetree/getHPathByID", data);
+}
+
+function request(url, data, method = "POST") {
+	return new Promise((resolve, reject) => {
+		if (method.toUpperCase() == "POST") {
+			fetch(url, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+				// body:data
+			})
+				.then(handleResponse)
+				.then((data) => resolve(data))
+				.then((error) => reject(error));
+		} else {
+			fetch(url)
+				.then(handleResponse)
+				.then((data) => resolve(data))
+				.then((error) => reject(error));
+		}
+	});
+
+	function handleResponse(response) {
+		let contentType = response.headers.get("content-type");
+		if (contentType.includes("application/json")) {
+			return handleJSONResponse(response);
+		} else if (contentType.includes("text/html")) {
+			return handleTextResponse(response);
+		} else {
+			throw new Error(`Sorry, content-type ${contentType} not supported`);
+		}
+	}
+
+	function handleJSONResponse(response) {
+		return response.json().then((json) => {
+			if (response.ok) {
+				return json;
+			} else {
+				return Promise.reject(
+					Object.assign({}, json, {
+						status: response.status,
+						statusText: response.statusText,
+					})
+				);
+			}
+		});
+	}
+	function handleTextResponse(response) {
+		return response.text().then((text) => {
+			if (response.ok) {
+				return text;
+			} else {
+				return Promise.reject({
+					status: response.status,
+					statusText: response.statusText,
+					err: text,
+				});
+			}
+		});
+	}
+}
+
+function addRenderNoteRoute() {
+	const list = document.querySelectorAll(
+		".render-node .protyle-wysiwyg__embed"
+	);
+
+	list.forEach((e) => {
+		const id = e.dataset.id;
+		getHPathByPath({
+			id,
+		}).then((res) => {
+			const p = document.createElement("p");
+			p.innerText = res.data;
+			e.prepend(p);
+		});
+	});
+}
+
+setTimeout(addRenderNoteRoute, 2000);
